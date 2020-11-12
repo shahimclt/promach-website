2bea032630fbb1c38af484edaf7f08ad73e4d852
M Shahim <shahim.calicut@gmail.com>
2020-11-12 23:33:53 +0530

    add mail function

--- promach_career.php
+++ promach_career.php
@@ -1,4 +1,10 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
   
 if($_POST) {
     $visitor_name = "";
@@ -50,13 +56,38 @@ if($_POST) {
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $visitor_email . "\r\n";
       
-    if(mail($recipient, $email_title, $email_body, $headers)) {
-        echo "<p>Thank you for contacting us, $visitor_name. We will get in touch with you shortly.</p>";
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-    }
+    // if(mail($recipient, $email_title, $email_body, $headers)) {
+    //     echo "<p>Thank you for contacting us, $visitor_name. We will get in touch with you shortly.</p>";
+    // } else {
+    //     echo '<p>We are sorry but the email did not go through.</p>';
+    // }
 
-    mail($recipient_cc, $email_title, $email_body, $headers);
+    // mail($recipient_cc, $email_title, $email_body, $headers);
+
+    $mail = new PHPMailer(true);
+
+    $mail->setFrom('website@promach.in', 'Website Mailer');
+    //Set an alternative reply-to address
+    $mail->addReplyTo('website@promach.in', 'Website Mailer');
+    //Set who the message is to be sent to
+    $mail->addAddress($recipient);
+    $mail->addAddress($recipient_cc);
+    //Set the subject line
+    $mail->Subject = $email_title;
+    //Read an HTML message body from an external file, convert referenced images to embedded,
+    //convert HTML into a basic plain-text alternative body
+    $mail->msgHTML($email_body);
+    //Replace the plain text body with one created manually
+    // $mail->AltBody = 'This is a plain-text message body';
+    //Attach an image file
+    // $mail->addAttachment('images/phpmailer_mini.png');
+
+    //send the message, check for errors
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message sent!';
+    }
       
 } else {
     echo '<p>Something went wrong</p>';
